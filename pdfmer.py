fcb5cac4080b624a2d893f892450bb4dec365025
Nithin <79245468+NithinGannarpu@users.noreply.github.com>
2023-03-24 23:07:48 +0530

    Update pdfmer.py

--- pdfmer.py
+++ pdfmer.py
@@ -1,12 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
 from PyPDF2 import PdfWriter
+import PyPDF2
 import tkinter.font as tkFont
+import pyttsx3
 import os
 
 win=Tk()
 win.title("Pdfmerger")
-win.geometry('629x500')
+win.geometry('790x500')
 win.iconbitmap('pdffavicon.ico')
 win.configure(bg='#a2c892')
 fnt=tkFont.Font(size=28)
@@ -37,14 +39,20 @@ def merge():
 
 
 
-def openit():
-    mer="mergedpdf.pdf"
+def readpdf():
+   
+    mer=output.get()+".pdf"
     if(mer in os.listdir()):
-        open("mergedpdf.pdf","rb")
-    else:
-        messagebox.showinfo("No pdf","Please merge pdf first")
+        book = open(mer,'rb')
+        reader = PyPDF2.PdfReader(book)
 
+        speaker = pyttsx3.init()
 
+        page = reader.pages[0]
+        text = page.extract_text()
+        speaker.say(text)
+        speaker.runAndWait()
+    
 
 
 name1=Label(win,text="Address of file 1",bg='#74a273')
@@ -66,12 +74,14 @@ output.grid(row=3,column=1,padx=10,pady=10)
 
 
 
-
 mergebtn=Button(win,text="Merge",command=merge,bg='#86b985')
 mergebtn.grid(row=4,column=0,padx=10,pady=10)
 
-openbtn=Button(win,text="Open merged pdf",command=openit,bg='#86b985')
-openbtn.grid(row=4,column=1,padx=10,pady=10)
+
+
+
+readbtn=Button(win,text="Read the merged file",command=readpdf,bg='#86b985')
+readbtn.grid(row=4,column=2,padx=10,pady=10)
 
 lb=Label(win,text="If you want to merge new pdf's please rename the existing 'mergedpdf.pdf' into something else",wraplength=500,font=fnt,bg='#587f5a',fg='#5e9b6f')
 lb.grid(row=5,column=1,padx=10)
